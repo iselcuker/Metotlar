7e13f2936a94c2f8533157f08b849ad17f490444
90505 <90505@DESKTOP-R6T1RN8>
2021-09-24 14:12:28 +0300

    Add project files.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler,"+" "+ urun.Adi +" "+"Sepete Eklendi...");
+        }
+
+        public void Ekle2(string urunAdi,string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler," + " " + urunAdi + " " + "Sepete Eklendi...");
+        }
+    }
+}
